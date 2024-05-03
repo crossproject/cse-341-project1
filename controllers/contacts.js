8fe7d219625b9c173a771dfda564f28c3668a3d4
crossproject <dnnyparra@gmail.com>
2024-05-02 21:43:26 -0300

    Finished W2 Step 2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags=["Contacts"]
     const result = await mongodb.getDatabase().db().collection("contacts").find();
     result.toArray().then((contacts) => {
         res.setHeader("Content-Type", "application/json");
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+  //#swagger.tags=["Contacts"]
     const userId = new ObjectId (req.params.id);
     const result = await mongodb.getDatabase().db().collection("contacts").find({_id: userId});
     result.toArray().then((contacts) => {
@@ -18,8 +20,8 @@ const getSingle = async (req, res) => {
     })
 };
 
-// show error
 const createContact = async (req, res) => {
+  //#swagger.tags=["Contacts"]
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -37,6 +39,7 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
+  //#swagger.tags=["Contacts"]
   const contactId = new ObjectId (req.params.id);
   const contact = {
     firstName: req.body.firstName,
@@ -55,6 +58,7 @@ const updateContact = async (req, res) => {
 };
 
 const deleteContact = async (req, res) => {
+  //#swagger.tags=["Contacts"]
   const contactId = new ObjectId (req.params.id);
   const response = await mongodb.getDatabase().db().collection("contacts").deleteOne({_id: contactId});
 
